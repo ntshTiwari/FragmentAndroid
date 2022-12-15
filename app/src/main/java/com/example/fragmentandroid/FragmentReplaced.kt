fda2fb1dde1c2dc253b6849aec219cace589f31a
Nitesh Kumar <niteshk@geekyants.com>
2022-12-15 19:25:14 +0530

    feat: Added replace an saveBackStack options

---
+++ app/src/main/java/com/example/fragmentandroid/FragmentReplaced.kt
@@ -0,0 +1,7 @@
+package com.example.fragmentandroid
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FragmentReplaced: Fragment(R.layout.activity_fragment_new)
--- app/src/main/java/com/example/fragmentandroid/MainActivity.kt
+++ app/src/main/java/com/example/fragmentandroid/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.fragmentandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +22,32 @@ class MainActivity : AppCompatActivity() {
                 add<FragmentOne>(R.id.fragment_container_view, args = bundle)
             }
         }
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            replaceFragment()
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            rollback()
+        }
     }
+
+    fun replaceFragment() {
+        /// this way we create a transaction
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<FragmentReplaced>(R.id.fragment_container_view, "We can add a random tag like this")
+            /// this way we save this transaction
+            addToBackStack("replacement")
+        }
+    }
+
+    fun rollback() {
+        /// we can restore transactions like this
+        supportFragmentManager.saveBackStack("replacement")
+    }
+
+    /// we can get a fragment by tag
+//    val fragment: FragmentReplaced =
+//        supportFragmentManager.findFragmentByTag("We can add a random tag like this") as FragmentReplaced
 }
