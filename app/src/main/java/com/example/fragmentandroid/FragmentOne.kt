f8600262abf6a5a63d9fcf6e2362bf0cc72efd14
Nitesh Kumar <niteshk@geekyants.com>
2022-12-15 18:55:47 +0530

    feat: Added code to pass data to a fragment

--- app/src/main/java/com/example/fragmentandroid/FragmentOne.kt
+++ app/src/main/java/com/example/fragmentandroid/FragmentOne.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class FragmentOne: Fragment(R.layout.activity_fragment_demo)
-
+//class FragmentOne: Fragment(R.layout.activity_fragment_demo)
+///// they both are same
 //class FragmentOne: Fragment() {
 //    override fun onCreateView(
 //        inflater: LayoutInflater,
@@ -18,3 +19,21 @@ class FragmentOne: Fragment(R.layout.activity_fragment_demo)
 ////        return super.onCreateView(inflater, container, savedInstanceState)
 //    }
 //}
+
+class FragmentOne: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_fragment_demo, container, false)
+//        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val someInt = requireArguments().getInt("some_int")
+        val newInt = requireArguments().getInt("new_int")
+        view.findViewById<TextView>(R.id.textView1).text = "$someInt $newInt"
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/fragmentandroid/MainActivity.kt
+++ app/src/main/java/com/example/fragmentandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragmentandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 
@@ -10,10 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /// beacuse of this it will only get created at the start, and not everytime
         if(savedInstanceState == null){
+            val bundle = bundleOf("some_int" to 0, "new_int" to 1)
+
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<FragmentOne>(R.id.fragment_container_view)
+                add<FragmentOne>(R.id.fragment_container_view, args = bundle)
             }
         }
     }
